fbed165c4db847b45babec7ffaf1e614c0471843
Arrielle Kooiman <arrielle.kooiman@gmail.com>
2017-03-09 15:09:28 -0600

    client side of feature is just about done

--- public/scripts/controllers/warehouse.controller.js
+++ public/scripts/controllers/warehouse.controller.js
@@ -1,5 +1,8 @@
 myApp.controller('WarehouseController', function(){
   console.log('Warehouse Controller was loaded');
+
   var self = this;
-  self.someMessage = 'This site is amazing!!';
+  self.dummyData = [{warehouse: 'alpha', fulfillment_days: 2}, {warehouse: 'beta', fulfillment_days: 3}, {warehouse: 'delta', fulfillment_days: 4}, {warehouse: 'gamma', fulfillment_days: 4}, {warehouse: 'epsilion', fulfillment_days: 5}];
+  console.log(self.dummyData);
+
 });
