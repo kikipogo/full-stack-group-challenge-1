7ffb76cab322f18d104e1d25498925a423c7492a
Jonny Holupchinski <jonholup@gmail.com>
2017-03-09 15:08:43 -0600

    base controller w/ dummy data appending to customer.html

--- public/scripts/controllers/customer.controller.js
+++ public/scripts/controllers/customer.controller.js
@@ -1,5 +1,14 @@
 myApp.controller('CustomerController', function(){
   console.log('Customer Controller was loaded');
+
+  var dummyData = [
+    {first_name: 'Lisa', last_name: 'Bonet'},
+    {first_name: 'Charles', last_name: 'Darwin'},
+    {first_name: 'George', last_name: 'Foreman'},
+    {first_name: 'Lucy', last_name: 'Liu'}
+  ];
+
   var self = this;
   self.someMessage = 'This site is amazing!!';
+  self.dummyData = dummyData;
 });
