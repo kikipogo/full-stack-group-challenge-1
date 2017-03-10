5f1b56f97ec483999fc4d397e7f5eabfcde164aa
Christine Pogatchnik <christinepogatchnik@gmail.com>
2017-03-10 10:05:39 -0600

    order

--- public/scripts/controllers/order.controller.js
+++ public/scripts/controllers/order.controller.js
@@ -1,5 +1,5 @@
 myApp.controller('OrderController', function(){
   console.log('Order Controller was loaded');
   var self = this;
-  self.someMessage = 'This site is amazing!!';
+  self.dummyData =[{order_date: 2010-03-05, description: 'toothbrush', street: '1 Main St', city: 'Detroit', state: 'MI', zip: 31111, address_type: 'home', first_name: 'Lisa', last_name: 'Bonet'}];
 });
